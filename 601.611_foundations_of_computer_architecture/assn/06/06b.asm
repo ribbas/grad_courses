9e006c9f8a2dae568deac6cee3f5a210c7c3c0d0
sabbirahm3d <sab61r.a@gmail.com>
2021-03-17 14:33:06 -0400

    finish 611 HW7

--- 601.611_foundations_of_computer_architecture/assn/06/06b.asm
+++ 601.611_foundations_of_computer_architecture/assn/06/06b.asm
@@ -1,9 +1,1 @@
-label_1:
-    beq $zero, $zero, label_2
-    j label_3
-    addi $t0, $t1, 100
-    sub $t0, $t1, $t2
-label_2:
-    beq $zero, $zero, label_1
-label_3:
-    addi $t0, $t1, 100
+addi $t9, $t9, 0
