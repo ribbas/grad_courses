03124f647fbe1393aac23a6afe5af1b6071e490d
ribbas <sab61r.a@gmail.com>
2021-09-12 15:49:56 -0400

    remove Java ans swift versions of 707 assn prog, fix imports

---
+++ 707_software_patterns/assn/01/prog/src/Document.hpp
@@ -0,0 +1,35 @@
+#ifndef DOCUMENT_H
+#define DOCUMENT_H
+
+#include <string>
+
+#include "Node.hpp"
+
+namespace dom {
+class Element;
+class Text;
+class Attr;
+class NodeList;
+
+class Document : public virtual Node {
+public:
+  virtual Element *createElement(const std::string &tagName) = 0;
+  virtual Text *createTextNode(const std::string &data) = 0;
+  virtual Attr *createAttribute(const std::string &name) = 0;
+  virtual Element *getDocumentElement() = 0;
+};
+}; // namespace dom
+
+class Document_Impl : public virtual dom::Document, public virtual Node_Impl {
+public:
+  Document_Impl(void);
+
+  virtual ~Document_Impl();
+
+  virtual dom::Element *createElement(const std::string &tagName);
+  virtual dom::Text *createTextNode(const std::string &data);
+  virtual dom::Attr *createAttribute(const std::string &name);
+  virtual dom::Element *getDocumentElement();
+};
+
+#endif // DOCUMENT_H
---
+++ 707_software_patterns/assn/01/prog/src/XMLSerializer.hpp
@@ -0,0 +1,21 @@
+#include <fstream>
+#include <ios>
+#include <string>
+
+#include "Node.hpp"
+
+class XMLSerializer {
+private:
+  std::fstream file;
+  int indentationLevel;
+
+  virtual void prettyIndentation();
+
+public:
+  XMLSerializer(const std::string &filename)
+      : indentationLevel(0), file(filename.c_str(), std::ios_base::out) {}
+  ~XMLSerializer() {}
+
+  virtual void serializePretty(dom::Node *node);
+  virtual void serializeMinimal(dom::Node *node);
+};
