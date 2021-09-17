f3411c37485b4ef5282cd559522e8a8d3c57f3b6
ribbas <sab61r.a@gmail.com>
2021-09-17 16:10:07 -0400

    I think I'm done with 629 assn 2

--- 629_programming_languages/assn/02/calc.py
+++ 629_programming_languages/assn/02/calc.py
@@ -1,79 +1,56 @@
-# -----------------------------------------------------------------------------
-# calc.py
-#
-# A simple calculator with variables.
-# -----------------------------------------------------------------------------
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+from ply import lex, yacc
+
+# tokens supported by the script
 tokens = (
-    "NAME",
     "NUMBER",
     "PLUS",
     "MINUS",
     "TIMES",
     "DIVIDE",
-    "EQUALS",
     "LPAREN",
     "RPAREN",
 )
 
-# Tokens
-
-t_PLUS = r"\+"
-t_MINUS = r"-"
-t_TIMES = r"\*"
-t_DIVIDE = r"/"
-t_EQUALS = r"="
-t_LPAREN = r"\("
-t_RPAREN = r"\)"
-t_NAME = r"[a-zA-Z_][a-zA-Z0-9_]*"
-
-
+# rule for parsing a NUMBER token
 def t_NUMBER(t):
     r"\d+"
     t.value = int(t.value)
     return t
 
 
-# Ignored characters
-t_ignore = " \t"
+# rules for parsing the operator tokens and the parenthesis
+t_PLUS = r"\+"
+t_MINUS = r"-"
+t_TIMES = r"\*"
+t_DIVIDE = r"/"
+t_LPAREN = r"\("
+t_RPAREN = r"\)"
 
+# ignored characters
+t_ignore = " "
 
-def t_newline(t):
-    r"\n+"
-    t.lexer.lineno += t.value.count("\n")
-
-
-def t_error(t):
-    print(f"Illegal character {t.value[0]!r}")
-    t.lexer.skip(1)
-
-
-# Build the lexer
-import ply.lex as lex
-
-lex.lex()
-
-# Precedence rules for the arithmetic operators
+# precedence rules for the arithmetic operators
 precedence = (
     ("left", "PLUS", "MINUS"),
     ("left", "TIMES", "DIVIDE"),
     ("right", "UMINUS"),
 )
 
-# dictionary of names (for storing variables)
-names = {}
+# exit on token errors
+def t_error(t):
+    exit(t)
 
 
-def p_statement_assign(p):
-    "statement : NAME EQUALS expression"
-    names[p[1]] = p[3]
-
-
+# grammar for EXPR
 def p_statement_expr(p):
     "statement : expression"
     print(p[1])
 
 
+# grammar for binary arithmetic operators
 def p_expression_binop(p):
     """expression : expression PLUS expression
     | expression MINUS expression
@@ -89,41 +66,31 @@ def p_expression_binop(p):
         p[0] = p[1] / p[3]
 
 
+# grammar for UNARY MINUS
 def p_expression_uminus(p):
     "expression : MINUS expression %prec UMINUS"
     p[0] = -p[2]
 
 
+# grammar for EXPR between LPAREN and RPAREN
 def p_expression_group(p):
     "expression : LPAREN expression RPAREN"
     p[0] = p[2]
 
 
+# grammar for NUMBER
 def p_expression_number(p):
     "expression : NUMBER"
     p[0] = p[1]
 
 
-def p_expression_name(p):
-    "expression : NAME"
-    try:
-        p[0] = names[p[1]]
-    except LookupError:
-        print(f"Undefined name {p[1]!r}")
-        p[0] = 0
-
-
+# exit on grammar errors
 def p_error(p):
-    print(f"Syntax error at {p.value!r}")
+    exit(p)
 
 
-import ply.yacc as yacc
-
+# build the lexer
+lex.lex()
 yacc.yacc()
 
-while True:
-    try:
-        s = input("calc > ")
-    except EOFError:
-        break
-    yacc.parse(s)
+yacc.parse("(-1 + (2 * 3 + 4) * -5) / 10")
