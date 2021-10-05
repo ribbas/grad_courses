918be782d8fa2d43b9ea3f9910c01577d18cfc10
ribbas <sab61r.a@gmail.com>
2021-10-04 20:07:59 -0400

    add 629 and 707 assn 5 prompts

---
+++ 707_software_patterns/assn/05/README.md
@@ -0,0 +1,18 @@
+# Assignment 5
+
+The assignment asked for the implementation of the following patterns to the program:
+
+- singleton
+- proxy
+
+## Source Files
+
+A combination of the original implementation, my implementation of Assignment 3 and the solution to the last assignment implementation were used. `Test.cpp` has been modified to add command line options for the Iterator and Builder patterns.
+
+To compile the assignment, the updated CMakeLists.txt file can be used.
+
+## Modifications for Assignment
+
+### Singleton
+
+### Proxy
---
+++ 707_software_patterns/assn/05/prog/src/Element.hpp
@@ -0,0 +1,73 @@
+#ifndef ELEMENT_H
+#define ELEMENT_H
+
+#include <string>
+
+#include "NamedNodeMap.hpp"
+#include "Node.hpp"
+
+namespace dom {
+class Attr;
+class NodeList;
+class Document;
+
+class Element : public virtual Node {
+public:
+    virtual ~Element() {}
+
+    //
+    // Serialization Data Extraction Strategy
+    //
+    virtual void appendValue(std::fstream*, WhitespaceStrategy*) = 0;
+    virtual void prependNodeName(std::fstream*, WhitespaceStrategy*) = 0;
+    virtual void prependNodeValue(std::fstream*, WhitespaceStrategy*) = 0;
+
+    virtual const std::string& getAttribute(const std::string&) = 0;
+    virtual Attr* getAttributeNode(const std::string&) = 0;
+    virtual NodeList* getElementsByTagName(const std::string&) = 0;
+    virtual const std::string& getTagName() = 0;
+    virtual bool hasAttribute(const std::string&) = 0;
+    virtual void removeAttribute(const std::string&) = 0;
+    virtual Attr* removeAttributeNode(Attr*) = 0;
+    virtual void setAttribute(const std::string&, const std::string&) = 0;
+    virtual Attr* setAttributeNode(Attr*) = 0;
+
+    virtual dom::NamedNodeMap* getAttributes() = 0;
+    virtual bool hasAttributes() = 0;
+};
+}; // namespace dom
+
+class Element_Impl : public virtual dom::Element, public virtual Node_Impl {
+private:
+    NamedNodeMap_Impl attributes;
+
+protected:
+    Element_Impl(const std::string&, dom::Document*);
+
+public:
+    friend class Document_Impl;
+
+    virtual ~Element_Impl();
+
+    //
+    // Serialization Data Extraction Strategy
+    //
+    virtual void appendValue(std::fstream*, WhitespaceStrategy*) override;
+    virtual void prependNodeName(std::fstream*, WhitespaceStrategy*) override;
+    virtual void prependNodeValue(std::fstream*, WhitespaceStrategy*) override;
+
+    virtual const std::string& getAttribute(const std::string&) override;
+    virtual dom::Attr* getAttributeNode(const std::string&) override;
+    virtual dom::NodeList* getElementsByTagName(const std::string&) override;
+    virtual const std::string& getTagName() override;
+    virtual bool hasAttribute(const std::string&) override;
+    virtual void removeAttribute(const std::string&) override;
+    virtual dom::Attr* removeAttributeNode(dom::Attr*) override;
+    virtual void setAttribute(const std::string&, const std::string&) override;
+    virtual dom::Attr* setAttributeNode(dom::Attr*) override;
+
+    virtual dom::NamedNodeMap* getAttributes() override;
+    virtual bool hasAttributes() override;
+};
+
+#endif // ELEMENT_H
