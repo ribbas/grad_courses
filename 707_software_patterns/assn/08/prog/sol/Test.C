eb7c5a8eb6cde849545253ce8835bb428a02f309
ribbas <sab61r.a@gmail.com>
2021-11-09 10:57:08 -0500

    add sols for 707 assn 07, 08 and 09

---
+++ 707_software_patterns/assn/08/prog/sol/Test.C
@@ -0,0 +1,297 @@
+#include <stdio.h>
+#include <fstream>
+#include <ios>
+#include "Attr.H"
+#include "Document.H"
+#include "Element.H"
+#include "Text.H"
+#include "XMLTokenizer.H"
+#include "XMLSerializer.H"
+#include "XMLValidator.H"
+#include "Builder.H"
+#include "Director.H"
+#include "StdOutObserver.H"
+
+void testTokenizer(int argc, char** argv);
+void testSerializer(int argc, char** argv);
+void testValidator(int argc, char** argv);
+void testIterator(int argc, char** argv);
+void testDirector(int argc, char** argv);
+
+void printUsage(void)
+{
+	printf("Usage:\n");
+	printf("\tTest t [file] ...\n");
+	printf("\tTest s [file1] [file2]\n");
+	printf("\tTest v [file]\n");
+	printf("\tTest i\n");
+	printf("\tTest d [file1] [file2]\n");
+}
+
+int main(int argc, char** argv)
+{
+	if (argc < 2)
+	{
+		printUsage();
+		exit(0);
+	}
+
+	switch(argv[1][0])
+	{
+	case 'T':
+	case 't':
+		testTokenizer(argc, argv);
+		break;
+	case 'S':
+	case 's':
+		testSerializer(argc, argv);
+		break;
+	case 'V':
+	case 'v':
+		testValidator(argc, argv);
+		break;
+	case 'I':
+	case 'i':
+		testIterator(argc, argv);
+		break;
+	case 'D':
+	case 'd':
+		testDirector(argc, argv);
+		break;
+	}
+}
+
+void testTokenizer(int argc, char** argv)
+{
+	dom::Document *	document	= new Document_Impl;
+
+	dom::Element *	element	= document->createElement("NewElement");
+	dom::Text *	text	= document->createTextNode("Text Data");
+	dom::Attr *	attr	= document->createAttribute("NewAttribute");
+
+	printf("Element Tag = '%s'\n", element->getTagName().c_str());
+	printf("Text Data = '%s'\n", text->getValue().c_str());
+	printf("Attr Name = '%s'\n", attr->getName().c_str());
+
+	element->setAttributeNode(attr);
+	printf("Element attribute '%s'='%s'\n", element->getTagName().c_str(), element->getAttribute("NewAttribute").c_str());
+
+	delete element;
+	delete text;
+	delete attr;
+	delete document;
+
+	for (int i = 2; i < argc; i++)
+	{
+		XMLTokenizer	tokenizer(argv[i]);
+
+		XMLTokenizer::XMLToken *	token	= 0;
+
+		printf("File:  '%s'\n", argv[i]);
+
+		do
+		{
+			delete	token;
+			token	= tokenizer.getNextToken();
+
+			printf("\tLine %d:  %s = '%s'\n", tokenizer.getLineNumber(),
+			  token->toString(), token->getToken().size() == 0 ? "" : token->getToken().c_str());
+
+		} while (token->getTokenType() != XMLTokenizer::XMLToken::NULL_TOKEN);
+
+		delete	token;
+	}
+}
+
+void testSerializer(int argc, char** argv)
+{
+	if (argc < 4)
+	{
+		printUsage();
+		exit(0);
+	}
+
+	//
+	// Create tree of this document:
+	// <? xml version="1.0" encoding="UTF-8"?>
+	// <document>
+	//   <element attribute="attribute value"/>
+	//   <element/>
+	//   <element attribute="attribute value" attribute2="attribute2 value">
+	//     Element Value
+	//   </element>
+	//   <element>
+	//   </element>
+	// </document>
+	//
+	dom::Document *	document	= new Document_Impl;
+	dom::Element *	root		= document->createElement("document");
+	document->appendChild(root);
+
+	dom::Element *	child		= document->createElement("element");
+	dom::Attr *	attr		= document->createAttribute("attribute");
+	attr->setValue("attribute value");
+	child->setAttributeNode(attr);
+	root->appendChild(child);
+
+	child				= document->createElement("element");
+	root->appendChild(child);
+
+	child				= document->createElement("element");
+	child->setAttribute("attribute", "attribute value");
+	child->setAttribute("attribute2", "attribute2 value");
+	dom::Text *	text		= document->createTextNode("Element Value");
+	child->appendChild(text);
+	root->appendChild(child);
+
+	child				= document->createElement("element");
+	root->appendChild(child);
+
+	//
+	// Serialize
+	//
+	std::fstream *	file	= 0;
+	XMLSerializer	xmlSerializer(file = new std::fstream(argv[2], std::ios_base::out));
+	xmlSerializer.serializePretty(document);
+	delete file;
+	XMLSerializer	xmlSerializer2(file = new std::fstream(argv[3], std::ios_base::out));
+	xmlSerializer2.serializeMinimal(document);
+	delete file;
+
+	// delete Document and tree.
+}
+
+void testValidator(int argc, char** argv)
+{
+	if (argc < 3)
+	{
+		printUsage();
+		exit(0);
+	}
+
+	//
+	// Create tree of this document:
+	// <? xml version="1.0" encoding="UTF-8"?>
+	// <document>
+	//   <element attribute="attribute value"/>
+	//   <element/>
+	//   <element attribute="attribute value" attribute2="attribute2 value">
+	//     Element Value
+	//   </element>
+	//   <element>
+	//   </element>
+	// </document>
+	//
+	// Schema for this document:
+	// document contains:  element
+	// element contains:  element
+	// element contains attributes:  attribute, attribute2
+	//
+	XMLValidator	xmlValidator;
+	ValidChildren *	schemaElement	= xmlValidator.addSchemaElement("");
+	schemaElement->addValidChild("document", false);
+	schemaElement	= xmlValidator.addSchemaElement("document");
+	schemaElement->addValidChild("element", false);
+	schemaElement	= xmlValidator.addSchemaElement("element");
+	schemaElement->addValidChild("element", false);
+	schemaElement->addValidChild("attribute", true);
+	schemaElement->addValidChild("attribute2", true);
+	schemaElement->setCanHaveText(true);
+
+	dom::Document *	document	= new DocumentValidator(new Document_Impl, &xmlValidator);
+	dom::Element *	root		= 0;
+	dom::Element *	child		= 0;
+	dom::Attr *	attr		= 0;
+
+	root		= new ElementValidator(document->createElement("document"), &xmlValidator);
+	document->appendChild(root);
+	child		= new ElementValidator(document->createElement("element"), &xmlValidator);
+	attr		= document->createAttribute("attribute");
+	attr->setValue("attribute value");
+	child->setAttributeNode(attr);
+	root->appendChild(child);
+	child		= new ElementValidator(document->createElement("element"), &xmlValidator);
+	root->appendChild(child);
+	child		= new ElementValidator(document->createElement("element"), &xmlValidator);
+	child->setAttribute("attribute", "attribute value");
+	child->setAttribute("attribute2", "attribute2 value");
+	dom::Text *	text		= document->createTextNode("Element Value");
+	child->appendChild(text);
+	root->appendChild(child);
+	child		= new ElementValidator(document->createElement("element"), &xmlValidator);
+	root->appendChild(child);
+
+	//
+	// Serialize
+	//
+	std::fstream *	file	= 0;
+	XMLSerializer	xmlSerializer(file = new std::fstream(argv[2], std::ios_base::out));
+	xmlSerializer.serializePretty(document);
+	delete file;
+
+	// delete Document and tree.
+}
+
+void testIterator(int argc, char** argv)
+{
+	//
+	// Create tree of this document:
+	// <? xml version="1.0" encoding="UTF-8"?>
+	// <document>
+	//   <element attribute="attribute value"/>
+	//   <element/>
+	//   <element attribute="attribute value" attribute2="attribute2 value">
+	//     Element Value
+	//   </element>
+	//   <element>
+	//   </element>
+	// </document>
+	//
+	dom::Document *	document	= new Document_Impl;
+	dom::Element *	root		= document->createElement("document");
+	document->appendChild(root);
+	printf("< 0x%08lx > (Last and highest node out of iterator)\n", (unsigned long )root);
+
+	dom::Element *	child		= document->createElement("element");
+	dom::Attr *	attr		= document->createAttribute("attribute");
+	attr->setValue("attribute value");
+	child->setAttributeNode(attr);
+	root->appendChild(child);
+	printf("  < 0x%08lx > (First node out of iterator)\n", (unsigned long )child);
+
+	child				= document->createElement("element");
+	root->appendChild(child);
+	printf("  < 0x%08lx > (Second node out of iterator)\n", (unsigned long )child);
+
+	child				= document->createElement("element");
+	child->setAttribute("attribute", "attribute value");
+	child->setAttribute("attribute2", "attribute2 value");
+	dom::Text *	text		= document->createTextNode("Element Value");
+	child->appendChild(text);
+	root->appendChild(child);
+	printf("  < 0x%08lx > (Fourth node out of iterator)\n", (unsigned long )child);
+	printf("    < 0x%08lx > (Third and deepest node out of iterator)\n", (unsigned long )text);
+
+	child				= document->createElement("element");
+	root->appendChild(child);
+	printf("  < 0x%08lx > (Fifth node out of iterator)\n", (unsigned long )child);
+
+	printf("\nDepth first iteration:\n");
+	dom::Iterator *	domIterator;
+	for (domIterator = document->createIterator(0); domIterator->hasNext();)
+		printf("node:  0x%08lx\n", (unsigned long )domIterator->next());
+	delete domIterator;
+
+	// delete Document and tree.
+}
+
+void testDirector(int argc, char** argv)
+{
+	dom::Document *	document	= new Document_Impl;
+	StdOutObserver	observer;
+	Builder		builder(document, &observer);
+	Director	director(argv[2], &builder);
+	std::fstream	file(argv[3], std::ios_base::out);
+	XMLSerializer	xmlSerializer(&file);
+	xmlSerializer.serializePretty(document);
+}
---
+++ 707_software_patterns/assn/09/prog/sol/Document.C
@@ -0,0 +1,141 @@
+#include "Document.H"
+#include "Element.H"
+#include "Text.H"
+#include "Attr.H"
+#include "NodeList.H"
+#include "XMLValidator.H"
+
+Document_Impl::Document_Impl(void) : Node_Impl("", dom::Node::DOCUMENT_NODE)
+{
+	Node_Impl::document	= this;
+}
+
+Document_Impl::~Document_Impl() {}
+
+void Document_Impl::serialize(std::fstream * writer, WhitespaceStrategy * whitespace)
+{
+	*writer << "<? xml version=\"1.0\" encoding=\"UTF-8\"?>";
+	whitespace->newLine(writer);
+	getDocumentElement()->serialize(writer, whitespace);
+}
+
+dom::Element *	Document_Impl::createElement(const std::string & tagName)
+{
+	return new Element_Impl(tagName, this);
+}
+
+dom::Text *	Document_Impl::createTextNode(const std::string & data)
+{
+	return new Text_Impl(data, this);
+}
+
+dom::Attr *	Document_Impl::createAttribute(const std::string & name)
+{
+	return new Attr_Impl(name, this);
+}
+
+dom::Element * Document_Impl::getDocumentElement()
+{
+	for (dom::NodeList::iterator i = getChildNodes()->begin(); i != getChildNodes()->end(); i++)
+		if (dynamic_cast<dom::Element *>(*i.operator->()) != 0)
+			return dynamic_cast<dom::Element *>(*i.operator->());
+
+	return 0;
+}
+
+dom::Iterator * Document_Impl::createIterator(dom::Node * node)
+{
+	return new DOMIterator(node, this);
+}
+
+DocumentValidator::DocumentValidator(dom::Document * _parent, XMLValidator * xmlValidator) :
+  Node_Impl("", dom::Node::DOCUMENT_NODE),
+  parent(_parent)
+{
+	schemaElement	= *xmlValidator->findSchemaElement("");
+}
+
+dom::Node * DocumentValidator::insertBefore(dom::Node * newChild, dom::Node * refChild)
+{
+	if (schemaElement == 0 || schemaElement->childIsValid(newChild->getNodeName(), false))
+		return parent->insertBefore(newChild, refChild);
+	else
+		throw dom::DOMException(dom::DOMException::VALIDATION_ERR, "Invalid root node " + newChild->getNodeName() + ".");
+}
+
+dom::Node * DocumentValidator::replaceChild(dom::Node * newChild, dom::Node * oldChild)
+{
+	if (schemaElement == 0 || schemaElement->childIsValid(newChild->getNodeName(), false))
+		return parent->replaceChild(newChild, oldChild);
+	else
+		throw dom::DOMException(dom::DOMException::VALIDATION_ERR, "Invalid root node " + newChild->getNodeName() + ".");
+}
+
+dom::Node * DocumentValidator::appendChild(dom::Node * newChild)
+{
+	if (schemaElement == 0 || schemaElement->childIsValid(newChild->getNodeName(), false))
+		return parent->appendChild(newChild);
+	else
+		throw dom::DOMException(dom::DOMException::VALIDATION_ERR, "Invalid root node " + newChild->getNodeName() + ".");
+}
+
+DOMIterator::DOMIterator(dom::Node * startWithNode, Document_Impl * document) :
+  firstNode(startWithNode == 0 ? (dom::Node * )document->getDocumentElement() : startWithNode)
+{
+	if (firstNode != 0)
+		for (dom::Node * node = firstNode; node->getChildNodes()->size() >0; node = *node->getChildNodes()->begin())
+		{
+			listStack.push(node->getChildNodes());
+			indexStack.push(0);
+		}
+}
+
+dom::Node * DOMIterator::elementAt(dom::NodeList * currentList, int currentIndex)
+{
+	int			i;
+	dom::NodeList::iterator	it;
+
+	for (i = 0, it = currentList->begin(); it != currentList->end() && i < currentIndex; i++, it++);
+
+	return *it;
+}
+
+bool DOMIterator::hasNext()
+{
+	return firstNode != 0;
+}
+
+dom::Node * DOMIterator::next()
+{
+	dom::NodeList *	currentList	= listStack.size() > 0 ? listStack.top() : 0;
+
+	if (currentList == 0)
+	{
+		dom::Node *	temp	= firstNode;
+		firstNode		= 0;
+		return temp;
+	}
+	else
+	{
+		int		currentIndex	= indexStack.top();
+		indexStack.pop();
+		dom::Node *	temp		= elementAt(currentList, currentIndex++);
+
+		if (currentIndex >= currentList->size())
+			listStack.pop();
+		else
+		{
+			indexStack.push(currentIndex);
+
+			for (dom::Node * node = elementAt(currentList, currentIndex);
+			  node->getChildNodes()->size() > 0;
+			  node = *node->getChildNodes()->begin())
+			{
+				listStack.push(node->getChildNodes());
+				indexStack.push(0);
+			}
+		}
+
+		return temp;
+	}
+}
