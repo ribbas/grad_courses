2f5bba8f52a140085d33800e2fab6467026f0c3b
ribbas <sab61r.a@gmail.com>
2022-02-06 16:06:44 -0500

    add special handling for carriage returns in 615 assn 2

--- 615_compiler_design_with_llvm/assn/02/ScannerWithFSM/Token.cpp
+++ 615_compiler_design_with_llvm/assn/02/ScannerWithFSM/Token.cpp
@@ -18,37 +18,53 @@ extern int IDtok;
 // format similar to: ("ed", ID),
 void Token::display(ostream& os) {
     ++tokens;
-	os << '(' << '"';
-	char* st = start;
-	char* fn = finish;
-	for (char* c = st; c < fn; ++c) {
-		if (*c == '\n') {
-			os << "\\n";
-		}
-		else {
-			os.put(*c);
-		}
-	}
-	os << '"' << ", ";
-	switch (kind) {
-	  case UNKNOWN   : os << "UNKNOWN"; ++UNKtok; break;
-	  case ELSE      : // os << "ELSE"; break;
-	  case END       : // os << "END"; break;
-	  case IF        : // os << "IF"; break;
-	  case RETURN    : // os << "RETURN"; break;
-	  case THEN      : // os << "THEN"; break;
-	                   os << "KW"; ++KWtok; break;
-	  case WS        : os << "WS"; ++WStok; break;
-	  case ID        : os << "ID"; ++IDtok; break;
-	  case NUM       : os << "NUM"; ++NUMtok; break;
-	  case SYM       : os << "SYM"; ++SYMtok; break;
-	}
-	os << "), ";
+    os << '(' << '"';
+    char* st = start;
+    char* fn = finish;
+    for (char* c = st; c < fn; ++c) {
+        if (*c == '\n' || *c == '\r') {
+            os << "\\n";
+        } else {
+            os.put(*c);
+        }
+    }
+    os << '"' << ", ";
+    switch (kind) {
+        case UNKNOWN:
+            os << "UNKNOWN";
+            ++UNKtok;
+            break;
+        case ELSE:   // os << "ELSE"; break;
+        case END:    // os << "END"; break;
+        case IF:     // os << "IF"; break;
+        case RETURN: // os << "RETURN"; break;
+        case THEN:   // os << "THEN"; break;
+            os << "KW";
+            ++KWtok;
+            break;
+        case WS:
+            os << "WS";
+            ++WStok;
+            break;
+        case ID:
+            os << "ID";
+            ++IDtok;
+            break;
+        case NUM:
+            os << "NUM";
+            ++NUMtok;
+            break;
+        case SYM:
+            os << "SYM";
+            ++SYMtok;
+            break;
+    }
+    os << "), ";
 
-	// only print 5 tokens per line
-	static int count = 0;
-	if (++count == 5) {
-		os << endl << flush;
-		count = 0;
-	}
+    // only print 5 tokens per line
+    static int count = 0;
+    if (++count == 5) {
+        os << endl << flush;
+        count = 0;
+    }
 }
--- 615_compiler_design_with_llvm/assn/02/ScannerWithFSM/getSymbol.cpp
+++ 615_compiler_design_with_llvm/assn/02/ScannerWithFSM/getSymbol.cpp
@@ -4,24 +4,24 @@
 // 1/2018
 // These routines help scan the input stream
 
-#include "getToken.h"
 #include "ASCII.h"
+#include "getToken.h"
+
 //#define and &&
 
 extern int SYMtests;
 extern int catches;
 
 // HW2 Symbol list:
-//      +  –  *  /  <  <=  ==  >=  >  =  (  )  #  “ //  /*  */864761
-
-
+//      +  ï¿½  *  /  <  <=  ==  >=  >  =  (  )  #  ï¿½ //  /*  */864761
 
 Token* getSymbol(char* input, char*& ch, char* startCh) {
-	try {
-		// TBD ... This is be filled in for HW2
-		return new Token(startCh, ch, SYM);
-	} catch ( ... ) {
+    try {
+        while (++SYMtests and ASCII[*ch] == 2)
+            ++ch;
+        return new Token(startCh, ch, SYM);
+    } catch (...) {
         ++catches;
-		return new Token(startCh, ch, SYM);
-	}
+        return new Token(startCh, ch, SYM);
+    }
 }
