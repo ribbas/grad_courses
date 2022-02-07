6876979bb9c5e2083e954eb4fe13cfdc6a3e02a9
ribbas <sab61r.a@gmail.com>
2022-02-07 12:37:05 -0500

    finish scannerwithfsm for 615 assn 2

---
+++ 615_compiler_design_with_llvm/assn/02/ScannerWithFSM/ASCII_SYM.h
@@ -0,0 +1,90 @@
+// Ascii_SYM.h
+//
+// by Sabbir Ahmed
+// 2/2022
+//
+// These tables combine to scan the following five keywords:
+//     else  end  if  return  then
+
+#ifndef ASCII_SYM_H_
+#define ASCII_SYM_H_
+// clang-format off
+
+// The SYM_IND table maps the input characters into 
+// the transitions to SYM_FSM, the next state index.
+const char SYM_Index[128] = {
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+
+	0, 0, 1, 2, 0, 0, 0, 0,  // ”, #
+	3, 4, 5, 6, 0, 7, 0, 8,  // (, ), *, +, –, /
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 9,10,11, 0,  // <, =, >
+
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0
+};
+
+// The SYM_State table maps the next FSM state
+// transition index. The first index is the
+// current state. The second index is the
+// index from the SYM_IND table.
+const char SYM_State[19][12] = { // indexed by [state][index]
+    //    ”   #   (   )   *   +   –   /   <   =   >
+    { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }, //  0 exit
+    { 0,  2,  3,  4,  5,  6,  8,  9, 10, 13, 15, 17 }, //  1 state can match
+                                                       // { ” | # | ( | ) | * | + | – | / | < | = | > }
+    { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }, //  2 state can match { ”$ }
+    { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }, //  3 state can match { #$ }
+    { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }, //  4 state can match { ($ }
+    { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }, //  5 state can match { )$ }
+    { 0,  0,  0,  0,  0,  0,  0,  0,  7,  0,  0,  0 }, //  6 state can match { *$ | */ }
+    { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }, //  7 state can match { */$ }
+    { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }, //  8 state can match { +$ }
+    { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }, //  9 state can match { –$ }
+    { 0,  0,  0,  0,  0, 11,  0,  0, 12,  0,  0,  0 }, // 10 state can match { /$ | // | /* }
+    { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }, // 11 state can match { /* }
+    { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }, // 12 state can match { // }
+    { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 14,  0 }, // 13 state can match { <$ | <= }
+    { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }, // 14 state can match { <=$ }
+    { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 16,  0 }, // 15 state can match { =$ | == }
+    { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }, // 16 state can match { ==$ }
+    { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 18,  0 }, // 17 state can match { >$ | >= }
+    { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }  // 18 state can match { >=$ }
+};
+
+// The SYM_Accept table identifies which states are
+// accepting states and which keyword they produce.
+const int SYM_Accept[] = {
+        0,  //  0
+        0,  //  1
+        1,  //  2 ”
+        2,  //  3 #
+        3,  //  4 (
+        4,  //  5 )
+        5,  //  6 *
+        6,  //  7 */
+        7,  //  8 +
+        8,  //  9 –
+        9,  // 10 /
+        10, // 11 /*
+        11, // 12 //
+        12, // 13 <
+        13, // 14 <=
+        14, // 15 =
+        15, // 16 ==
+        16, // 17 >
+        17  // 18 >=
+};
+
+// clang-format on
+#endif // ASCII_SYM_H_
--- 615_compiler_design_with_llvm/assn/02/ScannerWithFSM/getSymbol.cpp
+++ 615_compiler_design_with_llvm/assn/02/ScannerWithFSM/getSymbol.cpp
@@ -5,23 +5,57 @@
 // These routines help scan the input stream
 
 #include "ASCII.h"
+#include "ASCII_SYM.h"
 #include "getToken.h"
-
-//#define and &&
 
 extern int SYMtests;
 extern int catches;
 
 // HW2 Symbol list:
-//      +  �  *  /  <  <=  ==  >=  >  =  (  )  #  � //  /*  */864761
+// + – * / < <= == >= > = ( ) # ” // /* */
 
 Token* getSymbol(char* input, char*& ch, char* startCh) {
+    int accept = 0;
     try {
-        while (++SYMtests and ASCII[*ch] == 2)
-            ++ch;
-        return new Token(startCh, ch, SYM);
+        if (ASCII[*ch] == 2) {
+            int index = SYM_Index[*ch];
+            int state = SYM_State[1][index];
+            while (++SYMtests && ASCII[*ch] == 2 && state && index) {
+                accept = SYM_Accept[state];
+                ++ch;
+                index = SYM_Index[*ch];
+                state = SYM_State[state][index];
+            }
+            if (++SYMtests && accept) {
+                return new Token(startCh, ch, SYM);
+            }
+        }
+        return getID(input, ch, startCh);
+
+        // while (++SYMtests && ASCII[*ch] == 2) {
+
+        //     int index = SYM_Index[*ch];
+        //     int state = SYM_State[1][index];
+        //     ++ch;
+        //     std::cout << ch << '\n';
+        //     std::cout << " | ix " << index << " st " << state << '\n';
+        //     while (++SYMtests && ASCII[*ch] == 2 && state && index) {
+        //         accept = SYM_Accept[state];
+        //         ++ch;
+        //         index = SYM_Index[*ch];
+        //         state = SYM_State[state][index];
+        //     }
+        //     if (++SYMtests && accept) {
+        //         return new Token(startCh, ch, SYM);
+        //     }
+        // }
+        // std::cout << "done\n";
+        // return new Token(startCh, ch, SYM);
     } catch (...) {
         ++catches;
-        return new Token(startCh, ch, SYM);
+        if (++SYMtests and accept)
+            return new Token(startCh, ch, SYM);
+        ch = startCh;
+        return getID(input, ch, startCh);
     }
 }
