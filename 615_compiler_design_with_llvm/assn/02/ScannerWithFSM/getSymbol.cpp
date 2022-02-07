7ea964bc3067f31d4e16bac4f5ad523cf32e6651
ribbas <sab61r.a@gmail.com>
2022-02-07 13:50:39 -0500

    update getSymbol in 615 assn 2

--- 615_compiler_design_with_llvm/assn/02/ScannerWithFSM/getSymbol.cpp
+++ 615_compiler_design_with_llvm/assn/02/ScannerWithFSM/getSymbol.cpp
@@ -2,6 +2,9 @@
 //
 // by Allyn Shell
 // 1/2018
+// Modified by:
+// Sabbir Ahmed
+// 2/2022
 // These routines help scan the input stream
 
 #include "ASCII.h"
@@ -13,47 +16,28 @@ extern int catches;
 
 // HW2 Symbol list:
 // + – * / < <= == >= > = ( ) # ” // /* */
-
 Token* getSymbol(char* input, char*& ch, char* startCh) {
+
     int accept = 0;
+
     try {
-        if (ASCII[*ch] == 2) {
-            int index = SYM_Index[*ch];
-            int state = SYM_State[1][index];
-            while (++SYMtests && ASCII[*ch] == 2 && state && index) {
-                accept = SYM_Accept[state];
-                ++ch;
-                index = SYM_Index[*ch];
-                state = SYM_State[state][index];
-            }
-            if (++SYMtests && accept) {
-                return new Token(startCh, ch, SYM);
-            }
+
+        int index = SYM_Index[*ch];
+        int state = SYM_State[1][index];
+        while (++SYMtests && ASCII[*ch] == 2 && state && index) {
+            accept = SYM_Accept[state];
+            ++ch;
+            index = SYM_Index[*ch];
+            state = SYM_State[state][index];
+        }
+        if (++SYMtests && accept) {
+            return new Token(startCh, ch, SYM);
         }
         return getID(input, ch, startCh);
 
-        // while (++SYMtests && ASCII[*ch] == 2) {
-
-        //     int index = SYM_Index[*ch];
-        //     int state = SYM_State[1][index];
-        //     ++ch;
-        //     std::cout << ch << '\n';
-        //     std::cout << " | ix " << index << " st " << state << '\n';
-        //     while (++SYMtests && ASCII[*ch] == 2 && state && index) {
-        //         accept = SYM_Accept[state];
-        //         ++ch;
-        //         index = SYM_Index[*ch];
-        //         state = SYM_State[state][index];
-        //     }
-        //     if (++SYMtests && accept) {
-        //         return new Token(startCh, ch, SYM);
-        //     }
-        // }
-        // std::cout << "done\n";
-        // return new Token(startCh, ch, SYM);
     } catch (...) {
         ++catches;
-        if (++SYMtests and accept)
+        if (++SYMtests && accept)
             return new Token(startCh, ch, SYM);
         ch = startCh;
         return getID(input, ch, startCh);
