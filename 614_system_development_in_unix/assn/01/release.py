aaf6ec2378998738bfea351265a220f9e6898baf
ribbas <sab61r.a@gmail.com>
2022-02-17 22:27:07 -0500

    add gui tree files for 615 assn 3, revert release script in 614

--- 614_system_development_in_unix/assn/01/release.py
+++ 614_system_development_in_unix/assn/01/release.py
@@ -4,40 +4,41 @@
 #
 # Release Python script
 
-from os import listdir, path
+from pathlib import Path
 import subprocess
 import sys
+from typing import Union
 
 if __name__ == "__main__":
 
     if len(sys.argv) == 2:
 
-        command = ["tar", "-cvf"]
-        tarfile_name = ""
-        arch_files = None
-        cur_dir = path.dirname(path.abspath(__file__))
+        command: list = ["tar", "-cvf"]
+        tarfile_name: str = ""
+        arch_files: Union[Path, str] = None
+        cur_dir: Path = Path(__file__).parent
 
         if sys.argv[-1] == "-b":
-            confirm = raw_input("Generate a binary release? [Y/n]: ")
+            confirm = input("Generate a binary release? [Y/n]: ")
             if confirm.lower() == "y":
-                host_name = raw_input("Please enter a hostname: ")
+                host_name: str = input("Please enter a hostname: ")
+                tarfile_name: str = f"homework1_{host_name}.tar"
 
-                tarfile_name = "homework1_" + host_name + ".tar"
-                arch_files = [path.join(cur_dir, "bin")]
+                arch_files = [cur_dir / "bin"]
 
             else:
                 print("Binary release generation cancelled")
                 exit(0)
 
         elif sys.argv[-1] == "-s":
-            confirm = raw_input("Generate a source release? [Y/n]: ")
+            confirm = input("Generate a source release? [Y/n]: ")
             if confirm.lower() == "y":
+                tarfile_name = "homework1.tar"
 
-                make_clean_cmd = ["make", "clean"]
+                make_clean_cmd: list = ["make", "clean"]
                 subprocess.run(make_clean_cmd)
 
-                tarfile_name = "homework1.tar"
-                arch_files = listdir(cur_dir)
+                arch_files = list(i.name for i in cur_dir.iterdir())
 
             else:
                 print("Source release generation cancelled")
@@ -49,4 +50,5 @@ if __name__ == "__main__":
 
         command.append(tarfile_name)
         command.extend(arch_files)
+        print(command)
         subprocess.run(command)
