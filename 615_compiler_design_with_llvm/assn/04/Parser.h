bdbb5bfe2f8f1df3e565c6de4d9ad243e19dcd1f
ribbas <sab61r.a@gmail.com>
2022-02-18 14:18:14 -0500

    restructure assn folders in 615

---
+++ 615_compiler_design_with_llvm/assn/04/Parser.h
@@ -0,0 +1,31 @@
+// Parser.h
+// Allyn Shell
+// July 2019
+// Modified by:
+// TBD ... This is to be filled in for HW3 if you make changes
+// Modified date:
+// TBD ... This is to be filled in for HW3
+
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "Node.h"
+#include "SS_Cell.h"
+#include "Token.h"
+#include <string>
+
+using namespace std;
+
+class Parser {
+public:
+  static void parseEquation(char *&ch, SS_Cell *cell);
+  static Node *equation(char *&ch);
+  static Node *term(char *&ch);
+  static Node *factor(char *&ch);
+  static Node *parenExp(char *&ch);
+
+  static Token *match(char *&ch, TokenKind expected);
+  friend Token *getToken(char *&ch); // in Scanner.cpp
+};
+
+#endif // PARSER_H
