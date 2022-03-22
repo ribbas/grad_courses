de4b1052be7033da5426daf66e3608b04a098cbc
ribbas <sab61r.a@gmail.com>
2022-03-22 18:37:30 -0400

    init IR gen for if test for 615 compiler

--- 615_compiler_design_with_llvm/assn/06_CPIRGenIf/testFiles/DF-Test-2.c
+++
@@ -1,8 +0,0 @@
-/* pg. 493 The last declaration in a program must be a function declaration of the form void main(void). */
-
-void main(void) 
-{
-    errorBecauseLastIsNotMain();
-}
-
-void errorBecauseLastIsNotMain(void) { }
--- 615_compiler_design_with_llvm/assn/06_CPIRGenIf/testFiles/SA-Test-01.c
+++
@@ -1,6 +0,0 @@
-/* In a variable declaration, only the type specifier int may be used */
-
-void main(void)
-{
-    void x;
-}
