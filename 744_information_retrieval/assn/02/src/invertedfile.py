619d1c0420f4009aa922e51eb5fdeb74a88f8b42
ribbas <sab61r.a@gmail.com>
2022-09-16 12:36:28 -0400

    gonna start on the dict with offsets

--- 744_information_retrieval/assn/02/src/invertedfile.py
+++ 744_information_retrieval/assn/02/src/invertedfile.py
@@ -1,4 +1,4 @@
-from typing import Generator
+from typing import Generator, Iterable
 
 
 class InvertedFile:
@@ -7,6 +7,7 @@ class InvertedFile:
         self.term_doc_tf = content
         self.vocab = vocab
         self.inv_file_data: Generator[list[str | int], None, None] = []
+        self.col_split_list: list[tuple[int, int, int]] = []
 
     def convert_types(self, col_split_list: list[str]) -> tuple[int, int, int]:
         return (
@@ -15,14 +16,20 @@ class InvertedFile:
             int(col_split_list[2]),
         )
 
+    def map_to_int(self) -> list[tuple[int, int, int]]:
+
+        return [
+            self.convert_types(i.split(" "))
+            for i in self.term_doc_tf.split("\n")[:-1]
+        ]
+
     def ingest(self) -> None:
 
-        col_split_list: list[tuple[int, int, int]] = sorted(
-            (
-                self.convert_types(i.split(" "))
-                for i in self.term_doc_tf.split("\n")[:-1]
-            ),
-            key=lambda x: x[1],
-        )
-        col_split_list = sorted(col_split_list, key=lambda x: x[0])
-        print(col_split_list)
+        # Python implicitly performs radix sort via timsort
+        mapped_values: list[tuple[int, int, int]] = self.map_to_int()
+        mapped_values.sort()
+        # self.col_split_list = sorted(mapped_values, key=lambda x: x[0])
+        print(mapped_values)
+
+    def merge_postings(self) -> None:
+        pass
