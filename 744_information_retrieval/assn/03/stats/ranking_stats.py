911abf334a4c5909eff6ff198cd23678fe5b878a
ribbas <sab61r.a@gmail.com>
2022-10-13 17:14:14 -0400

    generated stats on outputs

---
+++ 744_information_retrieval/assn/03/stats/ranking_stats.py
@@ -0,0 +1,60 @@
+from collections import Counter
+from statistics import mean, median
+from pprint import pprint
+
+
+class Stats:
+    def __init__(self, filename: str) -> None:
+
+        self.elapsed_time = 0
+        self.filename = filename
+        self.rankings: list[dict[str, int | list[str]]] = []
+
+    def read_ranking_file(self) -> None:
+
+        line_num: int = 1
+        with open(self.filename) as fp:
+            line_data = []
+            for line in fp:
+                if line_num % 2:
+                    line_data = line[:-1].split(" ")
+                else:
+                    self.rankings.append(
+                        {
+                            "ndocs": int(line),
+                            "q": line_data,
+                            "nqueries": len(line_data),
+                        }
+                    )
+                line_num += 1
+
+        self.elapsed_time = float(line[:-2])
+
+    def count_q(self):
+
+        return Counter(
+            item for sublist in self.rankings for item in sublist["q"]
+        ).most_common(10)
+
+    def summary_stats(self, field: str):
+
+        agg = [i[field] for i in self.rankings]
+        return {
+            "mean": mean(agg),
+            "median": median(agg),
+        }
+
+    def generate(self):
+
+        self.read_ranking_file()
+        summary = {}
+        summary["time"] = self.elapsed_time
+        summary["queries"] = self.count_q()
+        summary["ndocs"] = self.summary_stats("ndocs")
+        summary["nqueries"] = self.summary_stats("nqueries")
+
+        return summary
+
+
+pprint(Stats("sahmed80-a-dump.txt").generate())
+pprint(Stats("sahmed80-b-dump.txt").generate())
